68702b2176a9277ed4d5be4121d47e40622fa8f3
Wilinton <wilintongabrielss1@gmail.com>
2025-05-08 16:06:21 -0300

    feat: adiciona redirecionamento para a página de login na rota raiz

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,6 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: 'register', component: RegistroComponent},
     {path: 'login', component: LoginComponent},
     {path: 'dashboard', component: DashboardComponent, canActivate: [authGuard]}
