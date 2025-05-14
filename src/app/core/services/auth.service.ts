768a0d62b2b99208010413c979becd6a10d187b2
Wilinton <wilintongabrielss1@gmail.com>
2025-05-14 16:00:11 -0300

    refactor: reorganiza importações do ambiente e restaura arquivo de configuração de ambiente de produção

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import { Observable, tap } from 'rxjs';
-import { environment } from '../environments/environment.prod'
+import { environment } from '../../environments/environment.prod'
 
 interface LoginPayload {
   username: string;
